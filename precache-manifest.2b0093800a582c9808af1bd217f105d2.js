6da3502aa318f0b6bcbbde0df7bc4868aae7ff24
Mark <poussard.mark@gmail.com>
2019-12-21 16:28:07 +0100

    Updates

---
+++ precache-manifest.2b0093800a582c9808af1bd217f105d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75882a42a7fb327b514e",
+    "url": "/irregular-verbs/static/js/runtime~main.75882a42.js"
+  },
+  {
+    "revision": "d3ab019cc32e2ea01890",
+    "url": "/irregular-verbs/static/js/main.d3ab019c.chunk.js"
+  },
+  {
+    "revision": "d88edf04b36961256868",
+    "url": "/irregular-verbs/static/js/1.d88edf04.chunk.js"
+  },
+  {
+    "revision": "d3ab019cc32e2ea01890",
+    "url": "/irregular-verbs/static/css/main.c4511979.chunk.css"
+  },
+  {
+    "revision": "56f15eb8194cad242f2831a004e82a5b",
+    "url": "/irregular-verbs/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/irregular-verbs/precache-manifest.068299509842270f8dfdfdf1907affca.js"
+  "/irregular-verbs/precache-manifest.2b0093800a582c9808af1bd217f105d2.js"
 );
 
 workbox.clientsClaim();
