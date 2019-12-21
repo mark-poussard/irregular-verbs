35c555f5a38d906b84bfcfe74461d255d912cd1f
Mark <poussard.mark@gmail.com>
2019-12-21 14:42:32 +0100

    Refactored factory to class

--- src/model/verb/VerbSection.ts
+++ src/model/verb/VerbSection.ts
@@ -20,32 +20,4 @@ export default class VerbSection{
     toCsv = () => {
         return `Section ${this.nbr}\n` +  this.verbs.map(v => v.toCsv()).join("\n");
     }
-
-    static generate = (verbs : Verb[], seed : number) => {
-        SeedRandom(seed.toString(), {global: true});
-        const verbSections : VerbSection[] = [];
-        const groups = VerbGroup.createGroups(verbs);
-        const sectionSize = Math.floor(verbs.length / NBR_SECTIONS); // Remainder ?
-        let sectionSizeRemainder = verbs.length % NBR_SECTIONS;
-        for(let i=0; i<NBR_SECTIONS; i++){
-            const sectionVerbs : Verb[] = [];
-            const localSectionSize = (sectionSizeRemainder-- > 0)
-                ? sectionSize + 1
-                : sectionSize;
-            let groupIdx = 0;
-            const basePickAllowance = 3;
-            while(sectionVerbs.length < localSectionSize){
-                if(groupIdx >= MAX_GROUP) groupIdx=0;
-                const nbrToPick = ((sectionVerbs.length + basePickAllowance) >= localSectionSize)
-                    ? (localSectionSize-sectionVerbs.length)
-                    : basePickAllowance;
-                const picked = groups[groupIdx].pick(nbrToPick);
-                sectionVerbs.push(...picked);
-                groupIdx ++;
-            }
-            verbSections.push(new VerbSection(i+1, sectionVerbs));
-        }
-        SeedRandom.resetGlobal();
-        return verbSections;
-    }
 }
